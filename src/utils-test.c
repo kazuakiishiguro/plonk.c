c688cb290f9b833d85ca8e042b5e9cc0b3ef68bb
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2024-09-04 08:35:23 +0900

    test_base

--- src/utils-test.c
+++ src/utils-test.c
@@ -2,14 +2,29 @@
 #include <stdio.h>
 #include "utils.h"
 
-int main() {
-    uint64_t value = 2;
-    uint64_t modulo = 13;
-    u64_fe field = u64_fe_new(value, modulo);
-    u64_fe result = u64_fe_pow(&field, 5);
-    assert(result.value == 6);
+#define MODULO 101
 
-    u64_fe inv = u64_fe_inv(&field);
-    assert(inv.value == 10);
-    return 0;
+u64_fe f101(uint64_t n) {
+  return u64_fe_new(n, MODULO);
+}
+
+void test_base() {
+  u64_fe zero = f101(0);
+  u64_fe one = f101(1);
+  u64_fe hundred = f101(100);
+  u64_fe four = f101(4);
+  u64_fe twelve = f101(12);
+
+  assert(f101(200).value == u64_fe_add(&hundred, &hundred).value);
+  assert(f101(100).value == u64_fe_sub(&zero, &one).value);
+  assert(f101(0).value == u64_fe_div(&one, &zero).value);
+
+  u64_fe four_div_twelve = u64_fe_div(&four, &twelve);
+  u64_fe r = u64_fe_mul(&twelve, &four_div_twelve);
+  assert(four.value == r.value);
+}
+
+int main() {
+  test_base();
+  return 0;
 }
