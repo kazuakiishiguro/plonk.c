d7383d8954232b55ce0b50a3c908b52b2c7acb5c
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2024-10-04 08:14:52 +0900

    cap LINE_EQ (pairing)

--- src/pairing-test.c
+++ src/pairing-test.c
@@ -2,28 +2,28 @@
 #include "pairing.h"
 
 void test_pairing_vectors() {
-  g1_p gen1 = g1_p_generator();
-  g2_p gen2 = g2_p_generator();
-  g1_p p = gen1;
-  g1_p r = g1_p_mul(&gen1, 4);
-  g2_p q = g2_p_mul(gen2, 3);
+  G1 gen1 = g1_generator();
+  G2 gen2 = g2_generator();
+  G1 p = gen1;
+  G1 r = g1_mul(&gen1, 4);
+  G2 q = g2_mul(gen2, 3);
   GF a = gf_new(5);
 
-  g1_p p_mul_a = g1_p_mul(&p, a.value);
-  gtp left = pairing(&p_mul_a, &q);
-  g2_p q_mul_a = g2_p_mul(q, a.value);
-  gtp right = pairing(&p, &q_mul_a);
+  G1 p_mul_a = g1_mul(&p, a.value);
+  GTP left = pairing(&p_mul_a, &q);
+  G2 q_mul_a = g2_mul(q, a.value);
+  GTP right = pairing(&p, &q_mul_a);
   assert(gtp_equal(&left, &right));
 
-  gtp left2 = pairing(&p_mul_a, &q);
-  gtp p_q = pairing(&p, &q);
-  gtp right2 = gtp_pow(&p_q, a.value);
+  GTP left2 = pairing(&p_mul_a, &q);
+  GTP p_q = pairing(&p, &q);
+  GTP right2 = gtp_pow(&p_q, a.value);
   assert(gtp_equal(&left2, &right2));
 
-  g1_p tmp = g1_p_add(&p, &r);
-  gtp p_plus_r = pairing(&tmp, &q);
-  gtp r_q = pairing(&r, &q);
-  gtp p_q_mul_r_q = gtp_mul(&p_q, &r_q);
+  G1 tmp = g1_add(&p, &r);
+  GTP p_plus_r = pairing(&tmp, &q);
+  GTP r_q = pairing(&r, &q);
+  GTP p_q_mul_r_q = gtp_mul(&p_q, &r_q);
   assert(gtp_equal(&p_plus_r, &p_q_mul_r_q));
 }
 
--- src/pairing.h
+++ src/pairing.h
@@ -6,7 +6,7 @@
 #include "g2.h"
 #include "gt.h"
 
-int gtp_equal(const gtp *p, const gtp *q) {
+int gtp_equal(const GTP *p, const GTP *q) {
   return gf_equal(p->a, q->a) && gf_equal(p->b, q->b);
 }
 
@@ -14,59 +14,59 @@ typedef struct {
   GF x;
   GF y;
   GF c;
-} line_eq;
+} LINE_EQ;
 
-line_eq line(const g1_p *a, const g1_p *b) {
+LINE_EQ line(const G1 *a, const G1 *b) {
   GF m = gf_sub(b->x, a->x);
   GF n = gf_sub(b->y, a->y);
 
   GF x = n;
   GF y = gf_neg(m);
   GF c = gf_sub(gf_mul(m, a->y), gf_mul(n, a->x));
-  line_eq l_eq = { .x = x, .y = y, .c = c };
+  LINE_EQ l_eq = { .x = x, .y = y, .c = c };
 
   return l_eq;
 }
 
-gtp pairing_f(uint64_t r, const g1_p *p, const g2_p *q) {
+GTP pairing_f(uint64_t r, const G1 *p, const G2 *q) {
   if (r == 1) {
     return gtp_new(f101(1), f101(0));
   } else if (r % 2 == 1) {
     uint64_t r_minus_1 = r - 1;
-    g1_p rp = g1_p_mul(p, r_minus_1);
-    line_eq l_eq = line(&rp, p);
+    G1 rp = g1_mul(p, r_minus_1);
+    LINE_EQ l_eq = line(&rp, p);
 
-    gtp prev = pairing_f(r_minus_1, p, q);
+    GTP prev = pairing_f(r_minus_1, p, q);
 
     GF x = gf_add(gf_mul(q->x, l_eq.x), l_eq.c);
     GF y = gf_mul(q->y, l_eq.y);
 
-    gtp gt_term = gtp_new(x, y);
+    GTP gt_term = gtp_new(x, y);
 
     return gtp_mul(&prev, &gt_term);
   } else {
     uint64_t r_div_2 = r / 2;
-    g1_p rp = g1_p_mul(p, r_div_2);
-    g1_p neg_rp = g1_p_neg(&rp);
-    g1_p two_neg_rp = g1_p_mul(&neg_rp, 2);
-    line_eq l_eq = line(&rp, &two_neg_rp);
+    G1 rp = g1_mul(p, r_div_2);
+    G1 neg_rp = g1_neg(&rp);
+    G1 two_neg_rp = g1_mul(&neg_rp, 2);
+    LINE_EQ l_eq = line(&rp, &two_neg_rp);
 
-    gtp prev = pairing_f(r_div_2, p, q);
-    gtp prev_sq = gtp_mul(&prev, &prev);
+    GTP prev = pairing_f(r_div_2, p, q);
+    GTP prev_sq = gtp_mul(&prev, &prev);
 
     GF x = gf_add(gf_mul(q->x, l_eq.x), l_eq.c);
     GF y = gf_mul(q->y, l_eq.y);
 
-    gtp gt_term = gtp_new(x, y);
+    GTP gt_term = gtp_new(x, y);
 
     return gtp_mul(&prev_sq, &gt_term);
   }
 }
 
-gtp pairing(const g1_p *g1, const g2_p *g2) {
+GTP pairing(const G1 *g1, const G2 *g2) {
   uint64_t p_order = MODULO;
-  uint64_t r = g1_p_generator_subgroup_size().value;
-  uint64_t k = g2_p_embedding_degree();
+  uint64_t r = g1_generator_subgroup_size().value;
+  uint64_t k = g2_embedding_degree();
 
   // Compute p^k mod r (if necessary)
   // Since p^k may be large, we need to handle this carefully
@@ -77,7 +77,7 @@ gtp pairing(const g1_p *g1, const g2_p *g2) {
 
   uint64_t exp = (p_pow_k - 1) / r;
 
-  gtp f = pairing_f(r, g1, g2);
+  GTP f = pairing_f(r, g1, g2);
 
   return gtp_pow(&f, exp);
 }
