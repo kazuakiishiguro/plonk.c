f3b7d5f9a2ea6dcbcc5e42d01b085597549733d8
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2024-10-04 07:52:11 +0900

    fix g1

--- src/g1.h
+++ src/g1.h
@@ -6,7 +6,7 @@
 #include "gf.h"
 
 typedef struct {
-  u8_fe x, y;
+  GF x, y;
   bool infinite;
 } g1_p;
 
@@ -27,7 +27,7 @@ bool g1_p_is_on_curve(const g1_p *point) {
   if (point->infinite) return true;
 
   // y^2 = x^3 + 3 mod MODULO
-  return u8_fe_equal(u8_fe_pow(point->y, 2), u8_fe_add(u8_fe_pow(point->x, 3), f101(3)));
+  return gf_equal(gf_pow(point->y, 2), gf_add(gf_pow(point->x, 3), f101(3)));
 }
 
 g1_p g1_p_identity() {
@@ -35,22 +35,22 @@ g1_p g1_p_identity() {
 }
 
 g1_p g1_p_double(const g1_p *a) {
-  if (a->infinite || u8_fe_equal(a->y, u8_fe_new(0))) return g1_p_identity();
+  if (a->infinite || gf_equal(a->y, gf_new(0))) return g1_p_identity();
 
-  u8_fe two = f101(2);
-  u8_fe three = f101(3);
-  u8_fe x_sq = u8_fe_mul(a->x, a->x);
-  u8_fe s = u8_fe_mul(three, x_sq);
-  u8_fe d = u8_fe_mul(two, a->y);
-  u8_fe m = u8_fe_div(s, d);
+  GF two = f101(2);
+  GF three = f101(3);
+  GF x_sq = gf_mul(a->x, a->x);
+  GF s = gf_mul(three, x_sq);
+  GF d = gf_mul(two, a->y);
+  GF m = gf_div(s, d);
 
-  u8_fe m_sq = u8_fe_mul(m, m);
-  u8_fe x_r = u8_fe_mul(two, a->x);
-  x_r = u8_fe_sub(m_sq, x_r);
-  u8_fe y_r = u8_fe_mul(three, a->x);
-  y_r = u8_fe_sub(y_r, m_sq);
-  y_r = u8_fe_mul(m, y_r);
-  y_r = u8_fe_sub(y_r, a->y);
+  GF m_sq = gf_mul(m, m);
+  GF x_r = gf_mul(two, a->x);
+  x_r = gf_sub(m_sq, x_r);
+  GF y_r = gf_mul(three, a->x);
+  y_r = gf_sub(y_r, m_sq);
+  y_r = gf_mul(m, y_r);
+  y_r = gf_sub(y_r, a->y);
 
   return g1_p_new(x_r.value, y_r.value);
 }
@@ -60,9 +60,9 @@ g1_p g1_p_add(const g1_p *a, const g1_p *b) {
   if (a->infinite) return *b;
   if (b->infinite) return *a;
 
-  if (u8_fe_equal(a->x, b->x)) {
-    u8_fe sum_y = u8_fe_add(a->y, b->y);
-    if (u8_fe_equal(sum_y, u8_fe_new(0))) {
+  if (gf_equal(a->x, b->x)) {
+    GF sum_y = gf_add(a->y, b->y);
+    if (gf_equal(sum_y, gf_new(0))) {
       // Pints are inverse of each other
       return g1_p_identity();
     } else {
@@ -71,20 +71,20 @@ g1_p g1_p_add(const g1_p *a, const g1_p *b) {
     }
   }
 
-  u8_fe s = u8_fe_sub(b->y, a->y);
-  u8_fe d = u8_fe_sub(b->x, a->x);
-  u8_fe m = u8_fe_mul(s, u8_fe_inv(d));
+  GF s = gf_sub(b->y, a->y);
+  GF d = gf_sub(b->x, a->x);
+  GF m = gf_mul(s, gf_inv(d));
 
-  u8_fe m_sq = u8_fe_mul(m, m);
-  u8_fe x_r = u8_fe_sub(u8_fe_sub(m_sq, a->x), b->x);
-  u8_fe y_r = u8_fe_sub(u8_fe_mul(m, u8_fe_sub(a->x, x_r)), a->y);
+  GF m_sq = gf_mul(m, m);
+  GF x_r = gf_sub(gf_sub(m_sq, a->x), b->x);
+  GF y_r = gf_sub(gf_mul(m, gf_sub(a->x, x_r)), a->y);
 
   return g1_p_new(x_r.value, y_r.value);
 }
 
 g1_p g1_p_neg(g1_p *a) {
   if (a->infinite) return *a;
-  u8_fe neg_y = u8_fe_neg(a->y);
+  GF neg_y = gf_neg(a->y);
   return g1_p_new(a->x.value, neg_y.value);
 }
 
@@ -102,7 +102,7 @@ g1_p g1_p_mul(const g1_p *point, uint64_t scalar) {
   return result;
 }
 
-u8_fe g1_p_generator_subgroup_size() {
+GF g1_p_generator_subgroup_size() {
   return f101(17);
 }
 
