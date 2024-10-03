617ac68232ae31f3668d03d0a81683b3c68e7908
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2024-10-04 08:07:10 +0900

    cap G1

--- src/g1-test.c
+++ src/g1-test.c
@@ -2,46 +2,46 @@
 #include "g1.h"
 
 #define ASSERT_G1(p1, p2)			      \
-    do {					      \
-        assert((p1).x.value == (p2).x.value);	      \
-        assert((p1).y.value == (p2).y.value);	      \
-    } while (0)
+  do {                                                \
+    assert((p1).x.value == (p2).x.value);	      \
+    assert((p1).y.value == (p2).y.value);	      \
+  } while (0)
 
 void test_g1_vectors() {
-     g1_p g = g1_p_generator();
-     g1_p neg_g = g1_p_neg(&g);
-     g1_p two = g1_p_add(&g, &g);
-     g1_p neg_two = g1_p_neg(&two);
-     g1_p three = g1_p_add(&two, &g);
-     g1_p four = g1_p_add(&two, &two);
-     g1_p neg_four = g1_p_neg(&four);
-     g1_p five = g1_p_add(&four, &g);
-     g1_p six = g1_p_add(&five, &g);
-     g1_p eight = g1_p_add(&four, &four);
-     g1_p neg_eight = g1_p_neg(&eight);
-     g1_p nine = g1_p_add(&eight, &g);
-     g1_p sixteen = g1_p_add(&eight, &eight);
-     g1_p neg_sixteen = g1_p_neg(&sixteen);
+  G1 g = g1_generator();
+  G1 neg_g = g1_neg(&g);
+  G1 two = g1_add(&g, &g);
+  G1 neg_two = g1_neg(&two);
+  G1 three = g1_add(&two, &g);
+  G1 four = g1_add(&two, &two);
+  G1 neg_four = g1_neg(&four);
+  G1 five = g1_add(&four, &g);
+  G1 six = g1_add(&five, &g);
+  G1 eight = g1_add(&four, &four);
+  G1 neg_eight = g1_neg(&eight);
+  G1 nine = g1_add(&eight, &g);
+  G1 sixteen = g1_add(&eight, &eight);
+  G1 neg_sixteen = g1_neg(&sixteen);
 
-     ASSERT_G1(g1_p_new(1, 99), neg_g);
-     ASSERT_G1(g1_p_new(68, 74), two);
-     ASSERT_G1(g1_p_new(68, 27), neg_two);
-     ASSERT_G1(g1_p_new(26, 45), three);
-     ASSERT_G1(g1_p_new(65, 98), four);
-     ASSERT_G1(g1_p_new(65, 3), neg_four);
-     ASSERT_G1(g1_p_new(12, 32), five);
-     ASSERT_G1(g1_p_new(18, 49), eight);
-     ASSERT_G1(g1_p_new(18, 52), neg_eight);
-     ASSERT_G1(g1_p_new(18, 52), nine);
-     ASSERT_G1(g1_p_new(1, 99), sixteen);
-     ASSERT_G1(g1_p_new(1, 2), neg_sixteen);
-     ASSERT_G1(g1_p_mul(&g, 1), g);
-     ASSERT_G1(g1_p_mul(&g, 2), two);
-     ASSERT_G1(g1_p_mul(&g, 2), two);
-     ASSERT_G1(g1_p_mul(&g, 6), six);
+  ASSERT_G1(g1_new(1, 99), neg_g);
+  ASSERT_G1(g1_new(68, 74), two);
+  ASSERT_G1(g1_new(68, 27), neg_two);
+  ASSERT_G1(g1_new(26, 45), three);
+  ASSERT_G1(g1_new(65, 98), four);
+  ASSERT_G1(g1_new(65, 3), neg_four);
+  ASSERT_G1(g1_new(12, 32), five);
+  ASSERT_G1(g1_new(18, 49), eight);
+  ASSERT_G1(g1_new(18, 52), neg_eight);
+  ASSERT_G1(g1_new(18, 52), nine);
+  ASSERT_G1(g1_new(1, 99), sixteen);
+  ASSERT_G1(g1_new(1, 2), neg_sixteen);
+  ASSERT_G1(g1_mul(&g, 1), g);
+  ASSERT_G1(g1_mul(&g, 2), two);
+  ASSERT_G1(g1_mul(&g, 2), two);
+  ASSERT_G1(g1_mul(&g, 6), six);
 }
 
 int main() {
-     test_g1_vectors();
-     return 0;
+  test_g1_vectors();
+  return 0;
 }
--- src/g1.h
+++ src/g1.h
@@ -8,10 +8,10 @@
 typedef struct {
   GF x, y;
   bool infinite;
-} g1_p;
+} G1;
 
-g1_p g1_p_new(uint64_t x_val, uint64_t y_val) {
-  g1_p point = {
+G1 g1_new(uint64_t x_val, uint64_t y_val) {
+  G1 point = {
     .x = f101(x_val),
     .y = f101(y_val),
     .infinite = false,
@@ -19,23 +19,23 @@ g1_p g1_p_new(uint64_t x_val, uint64_t y_val) {
   return point;
 }
 
-g1_p g1_p_generator() {
-  return g1_p_new(1, 2);
+G1 g1_generator() {
+  return g1_new(1, 2);
 }
 
-bool g1_p_is_on_curve(const g1_p *point) {
+bool g1_is_on_curve(const G1 *point) {
   if (point->infinite) return true;
 
   // y^2 = x^3 + 3 mod MODULO
   return gf_equal(gf_pow(point->y, 2), gf_add(gf_pow(point->x, 3), f101(3)));
 }
 
-g1_p g1_p_identity() {
-  return (g1_p){.infinite = true};
+G1 g1_identity() {
+  return (G1){.infinite = true};
 }
 
-g1_p g1_p_double(const g1_p *a) {
-  if (a->infinite || gf_equal(a->y, gf_new(0))) return g1_p_identity();
+G1 g1_double(const G1 *a) {
+  if (a->infinite || gf_equal(a->y, gf_new(0))) return g1_identity();
 
   GF two = f101(2);
   GF three = f101(3);
@@ -52,11 +52,11 @@ g1_p g1_p_double(const g1_p *a) {
   y_r = gf_mul(m, y_r);
   y_r = gf_sub(y_r, a->y);
 
-  return g1_p_new(x_r.value, y_r.value);
+  return g1_new(x_r.value, y_r.value);
 }
 
 // https://en.wikipedia.org/wiki/Elliptic_curve_point_multiplication#G1P_addition
-g1_p g1_p_add(const g1_p *a, const g1_p *b) {
+G1 g1_add(const G1 *a, const G1 *b) {
   if (a->infinite) return *b;
   if (b->infinite) return *a;
 
@@ -64,10 +64,10 @@ g1_p g1_p_add(const g1_p *a, const g1_p *b) {
     GF sum_y = gf_add(a->y, b->y);
     if (gf_equal(sum_y, gf_new(0))) {
       // Pints are inverse of each other
-      return g1_p_identity();
+      return g1_identity();
     } else {
       // Points are the same, perform doubling
-      return g1_p_double(a);
+      return g1_double(a);
     }
   }
 
@@ -79,30 +79,30 @@ g1_p g1_p_add(const g1_p *a, const g1_p *b) {
   GF x_r = gf_sub(gf_sub(m_sq, a->x), b->x);
   GF y_r = gf_sub(gf_mul(m, gf_sub(a->x, x_r)), a->y);
 
-  return g1_p_new(x_r.value, y_r.value);
+  return g1_new(x_r.value, y_r.value);
 }
 
-g1_p g1_p_neg(g1_p *a) {
+G1 g1_neg(G1 *a) {
   if (a->infinite) return *a;
   GF neg_y = gf_neg(a->y);
-  return g1_p_new(a->x.value, neg_y.value);
+  return g1_new(a->x.value, neg_y.value);
 }
 
-g1_p g1_p_mul(const g1_p *point, uint64_t scalar) {
-  g1_p result = g1_p_identity();
-  g1_p added = *point;
+G1 g1_mul(const G1 *point, uint64_t scalar) {
+  G1 result = g1_identity();
+  G1 added = *point;
 
   while(scalar > 0) {
     if (scalar & 1) {
-      result = g1_p_add(&result, &added);
+      result = g1_add(&result, &added);
     }
-    added = g1_p_double(&added);
+    added = g1_double(&added);
     scalar >>= 1;
   }
   return result;
 }
 
-GF g1_p_generator_subgroup_size() {
+GF g1_generator_subgroup_size() {
   return f101(17);
 }
 
