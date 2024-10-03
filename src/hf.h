59e8fdaa99cf7a618ed3dd096b94e444a45f384c
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2024-10-04 08:17:01 +0900

    cap HF

--- src/hf.h
+++ src/hf.h
@@ -11,58 +11,58 @@
 // Field element in HF (mod 17)
 typedef struct {
   uint8_t value;
-} hf_fe;
+} HF;
 
-hf_fe hf_fe_new(uint8_t x) {
-  hf_fe result;
+HF hf_new(uint8_t x) {
+  HF result;
   result.value = x % MODULO_HF;
   return result;
 }
 
-hf_fe f17(int64_t n) {
-  return hf_fe_new(n);
+HF f17(int64_t n) {
+  return hf_new(n);
 }
 
-bool hf_fe_equal(hf_fe a, hf_fe b) {
+bool hf_equal(HF a, HF b) {
   return a.value == b.value;
 }
 
-hf_fe hf_fe_add(hf_fe a, hf_fe b) {
-  return hf_fe_new((a.value + b.value) % MODULO_HF);
+HF hf_add(HF a, HF b) {
+  return hf_new((a.value + b.value) % MODULO_HF);
 }
 
-hf_fe hf_fe_sub(hf_fe a, hf_fe b) {
-  return hf_fe_new((a.value + MODULO_HF - b.value) % MODULO_HF);
+HF hf_sub(HF a, HF b) {
+  return hf_new((a.value + MODULO_HF - b.value) % MODULO_HF);
 }
 
-hf_fe hf_fe_mul(hf_fe a, hf_fe b) {
-  return hf_fe_new((a.value * b.value) % MODULO_HF);
+HF hf_mul(HF a, HF b) {
+  return hf_new((a.value * b.value) % MODULO_HF);
 }
 
-hf_fe hf_fe_pow(hf_fe base, uint32_t exponent) {
-  hf_fe result = hf_fe_new(1);
-  hf_fe b = base;
+HF hf_pow(HF base, uint32_t exponent) {
+  HF result = hf_new(1);
+  HF b = base;
   while (exponent > 0) {
     if (exponent % 2 == 1) {
-      result = hf_fe_mul(result, b);
+      result = hf_mul(result, b);
     }
-    b = hf_fe_mul(b, b);
+    b = hf_mul(b, b);
     exponent /= 2;
   }
   return result;
 }
 
-hf_fe hf_fe_inv(hf_fe a) {
+HF hf_inv(HF a) {
   if (a.value == 0) {
     fprintf(stderr, "Attempt to invert zero in finite field\n");
     exit(EXIT_FAILURE);
   }
   // Using Fermat's Little Theorem: a^(p-2) mod p
-  return hf_fe_pow(a, MODULO_HF - 2);
+  return hf_pow(a, MODULO_HF - 2);
 }
 
-hf_fe hf_fe_div(hf_fe a, hf_fe b) {
-  return hf_fe_mul(a, hf_fe_inv(b));
+HF hf_div(HF a, HF b) {
+  return hf_mul(a, hf_inv(b));
 }
 
 #endif // HF_H
