721bd967e907bb37fb7d5f596a735eab53e8a920
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2024-10-07 07:14:31 +0900

    constraints

--- src/constraints.h
+++ src/constraints.h
@@ -4,62 +4,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "gf.h"
+#include "hf.h"
 
 // the gate enforces the constraint:
 // q_l*a + q_r*b + q_o*c + q_m*a*b + q_c = 0
 typedef struct {
-  u8_fe q_l;
-  u8_fe q_r;
-  u8_fe q_o;
-  u8_fe q_m;
-  u8_fe q_c;
-} gate;
+  HF q_l;
+  HF q_r;
+  HF q_o;
+  HF q_m;
+  HF q_c;
+} GATE;
 
 typedef enum {
   COPYOF_A,
   COPYOF_B,
   COPYOF_C
-} copy_of_type;
+} COPY_OF_TYPE;
 
 // represents references to wire assignments in the circuit
 // (inputs and outputs)
 typedef struct {
-  copy_of_type type;
+  COPY_OF_TYPE type;
   size_t index;
-} copy_of;
+} COPY_OF;
 
 // holds vectors of gate coefficients and copy constraints.
 // provides methods to evaluate expressions and check
 // if a set of assignments satisfies the constraints
 typedef struct {
-  u8_fe *q_l;
-  u8_fe *q_r;
-  u8_fe *q_o;
-  u8_fe *q_m;
-  u8_fe *q_c;
+  HF *q_l;
+  HF *q_r;
+  HF *q_o;
+  HF *q_m;
+  HF *q_c;
   size_t num_gates;
 
-  copy_of *c_a;
-  copy_of *c_b;
-  copy_of *c_c;
+  COPY_OF *c_a;
+  COPY_OF *c_b;
+  COPY_OF *c_c;
   size_t num_constraints;
-} constraints;
+} CONSTRAINTS;
 
 // assignment and assignments structs represent individual and
 // collections of wire assignments in the circuit.
 typedef struct {
-  u8_fe a;
-  u8_fe b;
-  u8_fe c;
-} assignment;
+  HF a;
+  HF b;
+  HF c;
+} ASSIGNMENT;
 
 typedef struct {
-  u8_fe *a;
-  u8_fe *b;
-  u8_fe *c;
+  HF *a;
+  HF *b;
+  HF *c;
   size_t len;
-} assignments;
+} ASSIGNMENTS;
 
 typedef enum {
   EXPR_VAR,
@@ -67,57 +67,57 @@ typedef enum {
   EXPR_SUM,
   EXPR_SUB,
   EXPR_MUL
-} expr_type;
+} EXPR_TYPE;
 
 typedef struct expression {
-  expr_type type;
+  EXPR_TYPE type;
   union {
     const char *var_name; // for EXPR_VAR
-    u8_fe const_value;    // for EXPR_CONST
+    HF const_value;    // for EXPR_CONST
     struct {
       struct expression *left;
       struct expression *right;
     } binary; // for EXPR_SUM, EXPR_SUB, EXPR_MUL
   } data;
-} expression;
+} EXPRESSION;
 
-gate gate_new(u8_fe q_l, u8_fe q_r, u8_fe q_o, u8_fe q_m, u8_fe q_c) {
-  gate g = {q_l, q_r, q_o, q_m, q_c};
+GATE gate_new(HF q_l, HF q_r, HF q_o, HF q_m, HF q_c) {
+  GATE g = {q_l, q_r, q_o, q_m, q_c};
   return g;
 }
 
 // sum gate: a + b - c = 0
-gate gate_sum_a_b() {
-  return gate_new(u8_fe_one(), u8_fe_one(), u8_fe_neg(u8_fe_one()), u8_fe_zero(), u8_fe_zero());
+GATE gate_sum_a_b() {
+  return gate_new(hf_one(), hf_one(), hf_neg(hf_one()), hf_zero(), hf_zero());
 }
 
 // sub gate: a - b - c = 0
-gate gate_sub_a_b() {
-  return gate_new(u8_fe_one(), u8_fe_neg(u8_fe_one()), u8_fe_neg(u8_fe_one()), u8_fe_zero(), u8_fe_zero());
+GATE gate_sub_a_b() {
+  return gate_new(hf_one(), hf_neg(hf_one()), hf_neg(hf_one()), hf_zero(), hf_zero());
 }
 
 // mul gate: a * b - c = 0
-gate gate_mul_a_b() {
-  return gate_new(u8_fe_zero(), u8_fe_zero(), u8_fe_neg(u8_fe_one()), u8_fe_one(), u8_fe_zero());
+GATE gate_mul_a_b() {
+  return gate_new(hf_zero(), hf_zero(), hf_neg(hf_one()), hf_one(), hf_zero());
 }
 
 // binds a variable to a constant: a + q_c = 0
-gate gate_bind_a(u8_fe value) {
-  return gate_new(u8_fe_one(), u8_fe_zero(), u8_fe_zero(), u8_fe_zero(), value);
+GATE gate_bind_a(HF value) {
+  return gate_new(hf_one(), hf_zero(), hf_zero(), hf_zero(), value);
 }
 
 // Gate to bind variable to zero: c = 0
-gate gate_bind_to_zero() {
-  return gate_new(u8_fe_zero(), u8_fe_zero(), u8_fe_one(), u8_fe_zero(), u8_fe_zero());
+GATE gate_bind_to_zero() {
+  return gate_new(hf_zero(), hf_zero(), hf_one(), hf_zero(), hf_zero());
 }
 
-constraints constraints_new(gate *gates, size_t num_gates, copy_of *c_a, copy_of *c_b, copy_of *c_c, size_t num_constraints) {
-  constraints cons;
-  cons.q_l = (u8_fe *)malloc(num_gates * sizeof(u8_fe));
-  cons.q_r = (u8_fe *)malloc(num_gates * sizeof(u8_fe));
-  cons.q_o = (u8_fe *)malloc(num_gates * sizeof(u8_fe));
-  cons.q_m = (u8_fe *)malloc(num_gates * sizeof(u8_fe));
-  cons.q_c = (u8_fe *)malloc(num_gates * sizeof(u8_fe));
+CONSTRAINTS constraints_new(GATE *gates, size_t num_gates, COPY_OF *c_a, COPY_OF *c_b, COPY_OF *c_c, size_t num_constraints) {
+  CONSTRAINTS cons;
+  cons.q_l = (HF *)malloc(num_gates * sizeof(HF));
+  cons.q_r = (HF *)malloc(num_gates * sizeof(HF));
+  cons.q_o = (HF *)malloc(num_gates * sizeof(HF));
+  cons.q_m = (HF *)malloc(num_gates * sizeof(HF));
+  cons.q_c = (HF *)malloc(num_gates * sizeof(HF));
   cons.num_gates = num_gates;
 
   for (size_t i = 0; i < num_gates; i++) {
@@ -128,9 +128,9 @@ constraints constraints_new(gate *gates, size_t num_gates, copy_of *c_a, copy_of
     cons.q_c[i] = gates[i].q_c;
   }
 
-  cons.c_a = (copy_of *)malloc(num_constraints * sizeof(copy_of));
-  cons.c_b = (copy_of *)malloc(num_constraints * sizeof(copy_of));
-  cons.c_c = (copy_of *)malloc(num_constraints * sizeof(copy_of));
+  cons.c_a = (COPY_OF *)malloc(num_constraints * sizeof(COPY_OF));
+  cons.c_b = (COPY_OF *)malloc(num_constraints * sizeof(COPY_OF));
+  cons.c_c = (COPY_OF *)malloc(num_constraints * sizeof(COPY_OF));
   cons.num_constraints = num_constraints;
 
   for (size_t i = 0; i < num_constraints; i++) {
@@ -143,26 +143,26 @@ constraints constraints_new(gate *gates, size_t num_gates, copy_of *c_a, copy_of
 }
 
 
-bool constraints_satisfy(const constraints *c, const assignments *a) {
+bool constraints_satisfy(const CONSTRAINTS *c, const ASSIGNMENTS *a) {
   size_t n = c->num_constraints;
   for (size_t i = 0; i < n; i ++) {
     // compute lhs = (q_l[i] * a[i]) + (q_r[i] * b[i]) + (q_o[i] * c[i])
     //             + (q_m[i] * a[i] * b[i]) + q_c[i]
-    u8_fe lhs = u8_fe_zero();
-    u8_fe term1 = u8_fe_mul(c->q_l[i], a->a[i]);
-    u8_fe term2 = u8_fe_mul(c->q_r[i], a->b[i]);
-    u8_fe term3 = u8_fe_mul(c->q_o[i], a->c[i]);
-    u8_fe a_b = u8_fe_mul(a->a[i], a->b[i]);
-    u8_fe term4 = u8_fe_mul(c->q_m[i], a_b);
+    HF lhs = hf_zero();
+    HF term1 = hf_mul(c->q_l[i], a->a[i]);
+    HF term2 = hf_mul(c->q_r[i], a->b[i]);
+    HF term3 = hf_mul(c->q_o[i], a->c[i]);
+    HF a_b = hf_mul(a->a[i], a->b[i]);
+    HF term4 = hf_mul(c->q_m[i], a_b);
 
-    lhs = u8_fe_add(lhs, term1);
-    lhs = u8_fe_add(lhs, term2);
-    lhs = u8_fe_add(lhs, term3);
-    lhs = u8_fe_add(lhs, term4);
-    lhs = u8_fe_add(lhs, c->q_c[i]);
+    lhs = hf_add(lhs, term1);
+    lhs = hf_add(lhs, term2);
+    lhs = hf_add(lhs, term3);
+    lhs = hf_add(lhs, term4);
+    lhs = hf_add(lhs, c->q_c[i]);
 
     // check if lhs is zero
-    if (!u8_fe_equal(lhs, u8_fe_zero())) {
+    if (!hf_equal(lhs, hf_zero())) {
       return false;
     }
   }
@@ -170,7 +170,7 @@ bool constraints_satisfy(const constraints *c, const assignments *a) {
   return true;
 }
 
-void constraints_free(constraints *cons) {
+void constraints_free(CONSTRAINTS *cons) {
   free(cons->q_l);
   free(cons->q_r);
   free(cons->q_o);
@@ -188,13 +188,13 @@ typedef struct {
   char *names[MAX_VARS];
   size_t indices[MAX_VARS];
   size_t count;
-} var_map;
+} VAR_MAP;
 
-void var_map_init(var_map *vm) {
+void var_map_init(VAR_MAP *vm) {
   vm->count = 0;
 }
 
-size_t var_map_get_or_add(var_map *vm, const char *name) {
+size_t var_map_get_or_add(VAR_MAP *vm, const char *name) {
   for (size_t i = 0; i < vm->count; i++) {
     if (strcmp(vm->names[i], name) == 0)
       return vm->indices[i];
@@ -212,40 +212,40 @@ size_t var_map_get_or_add(var_map *vm, const char *name) {
   return vm->indices[vm->count - 1];
 }
 
-const char *var_map_get_name(var_map *vm, size_t index) {
+const char *var_map_get_name(VAR_MAP *vm, size_t index) {
   if (index < vm->count)
     return vm->names[index];
   return NULL;
 }
 
-void var_map_free(var_map *vm) {
+void var_map_free(VAR_MAP *vm) {
   for (size_t i = 0; i < vm->count; i++)
     free(vm->names[i]);
   vm->count = 0;
 }
 
 typedef struct {
-  gate *gates;
+  GATE *gates;
   size_t *a_indices;
   size_t *b_indices;
   size_t *c_indices;
   size_t num_gates;
   size_t capacity;
-} gate_list;
+} GATE_LIST;
 
-void gate_list_init(gate_list *gl) {
+void gate_list_init(GATE_LIST *gl) {
   gl->capacity = 10;
-  gl->gates = (gate *)malloc(gl->capacity * sizeof(gate));
+  gl->gates = (GATE *)malloc(gl->capacity * sizeof(GATE));
   gl->a_indices = (size_t *)malloc(gl->capacity * sizeof(size_t));
   gl->b_indices = (size_t *)malloc(gl->capacity * sizeof(size_t));
   gl->c_indices = (size_t *)malloc(gl->capacity * sizeof(size_t));
   gl->num_gates = 0;
 }
 
-void gate_list_append(gate_list *gl, gate g, size_t a_index, size_t b_index, size_t c_index) {
+void gate_list_append(GATE_LIST *gl, GATE g, size_t a_index, size_t b_index, size_t c_index) {
   if (gl->num_gates >= gl->capacity) {
     gl->capacity *= 2;
-    gl->gates = (gate *)realloc(gl->gates, gl->capacity * sizeof(gate));
+    gl->gates = (GATE *)realloc(gl->gates, gl->capacity * sizeof(GATE));
     gl->a_indices = (size_t *)realloc(gl->a_indices, gl->capacity * sizeof(size_t));
     gl->b_indices = (size_t *)realloc(gl->b_indices, gl->capacity * sizeof(size_t));
     gl->c_indices = (size_t *)realloc(gl->c_indices, gl->capacity * sizeof(size_t));
@@ -263,14 +263,14 @@ void gate_list_append(gate_list *gl, gate g, size_t a_index, size_t b_index, siz
   gl->num_gates++;
 }
 
-void gate_list_free(gate_list *gl) {
+void gate_list_free(GATE_LIST *gl) {
   free(gl->gates);
   free(gl->a_indices);
   free(gl->b_indices);
   free(gl->c_indices);
 }
 
-size_t eval_expr(expression *expr, var_map *vars, gate_list *gates) {
+size_t eval_expr(EXPRESSION *expr, VAR_MAP *vars, GATE_LIST *gates) {
   switch (expr->type) {
   case EXPR_VAR: {
     return var_map_get_or_add(vars, expr->data.var_name);
@@ -291,15 +291,15 @@ size_t eval_expr(expression *expr, var_map *vars, gate_list *gates) {
     snprintf(var_name, sizeof(var_name), "v%zu", n);
     var_map_get_or_add(vars, var_name);
 
-    gate g;
+    GATE gate;
     if (expr->type == EXPR_SUM) {
-      g = gate_sum_a_b();
+      gate = gate_sum_a_b();
     } else if (expr->type == EXPR_SUB) {
-      g = gate_sub_a_b();
+      gate = gate_sub_a_b();
     } else {
-      g = gate_mul_a_b();
+      gate = gate_mul_a_b();
     }
-    gate_list_append(gates, g, l, r, n);
+    gate_list_append(gates, gate, l, r, n);
     return n;
   }
   default:
