861dd0eaa774f9173222f1af4bd18cc24e7068cd
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2024-11-07 11:33:17 +0900

    add init and its test

--- src/plonk-test.c
+++ src/plonk-test.c
@@ -1,6 +1,18 @@
 #include <assert.h>
 #include "plonk.h"
 
+bool matrix_equal(const MATRIX *a, const MATRIX *b) {
+  if (a->m != b->m || a->n != b->n)
+    return false;
+  size_t size = a->n * a->n;
+  for (size_t i = 0; i < size; i++) {
+    if (!hf_equal(a->v[i], b->v[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
 void test_plonk_init() {
   // create the trusted setup
   GF secret = f101(2); // the toxic waste
@@ -15,6 +27,10 @@ void test_plonk_init() {
   for (uint8_t i = 0; i < h_len; i ++) {
     assert(hf_equal(plonk.h[i], hf_pow(hf_new(OMEGA_VALUE), i)));
   }
+
+  HF vals[] = {f17(13), f17(13), f17(13), f17(13), f17(13), f17(16), f17(4), f17(1), f17(13), f17(4), f17(13), f17(4), f17(13), f17(1), f17(4), f17(16)};
+  MATRIX expected = matrix_new(vals, h_len, h_len);
+  assert(matrix_equal(&plonk.h_pows_inv, &expected));
 }
 
 int main() {
--- src/plonk.h
+++ src/plonk.h
@@ -2,16 +2,19 @@
 #define PLONK_H
 
 #include <stdlib.h>
-
+#include "matrix.h"
 #include "poly.h"
 #include "srs.h"
 #include "hf.h"
 
 #define OMEGA_VALUE 4 // example value; should be a primitive roor of unity in the field
+#define K1_VALUE    2 // should not be in H
+#define K2_VALUE    3 // should not be in H or K1 * H
 
 typedef struct {
   SRS srs;
   HF *h;        // array of HF elements
+  MATRIX h_pows_inv;
   size_t h_len;
   HF *k1_h;     // array of k1 * h elements
   HF *k2_h;     // array of k2 * h elements
@@ -25,6 +28,8 @@ PLONK plonk_new(SRS srs, size_t n) {
 
   // init
   HF OMEGA = hf_new(OMEGA_VALUE);
+  HF K1 = hf_new(K1_VALUE);
+  HF K2 = hf_new(K2_VALUE);
 
   // compute h = [omega^0, omega^1, ...., omega^n]
   plonk.h = (HF *)malloc(plonk.h_len * sizeof(HF));
@@ -32,10 +37,54 @@ PLONK plonk_new(SRS srs, size_t n) {
     fprintf(stderr, "Memory allocation failed in plnk_new\n");
     exit(EXIT_FAILURE);
   }
-
   for (uint8_t i = 0; i < plonk.h_len; i++) {
     plonk.h[i] = hf_pow(OMEGA, i);
   }
+
+  // check K1 and K2 are not in H
+  for (size_t i = 0; i < plonk.h_len; i++) {
+    if (hf_equal(plonk.h[i], K1) || hf_equal(plonk.h[i], K2)) {
+      fprintf(stderr, "K1 or K2 is in H, which is not allowed\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  // compute h1_h: h1_h[i] = K1 * h[i]
+  // compute k2_h: k2_h[i] = K2 * h[i]
+  plonk.k1_h = (HF *)malloc(plonk.h_len * sizeof(HF));
+  plonk.k2_h = (HF *)malloc(plonk.h_len * sizeof(HF));
+  if (!plonk.k1_h || !plonk.k2_h) {
+    fprintf(stderr, "Memory allocation failed in plonk_new\n");
+    exit(EXIT_FAILURE);
+  }
+  // k1_h is a coset of H
+  for (size_t i = 0; i < plonk.h_len; i++) {
+    plonk.k1_h[i] = hf_mul(plonk.h[i], K1);
+  }
+  // check K2 is chosen so that it is neither an element of H nor k1_h[i]
+  for (size_t i = 0; i < plonk.h_len; i++) {
+    if (hf_equal(plonk.k1_h[i], K2)) {
+      fprintf(stderr, "K1 or K2 is in H, which is not allowed\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+  // k2_h is a coset of H
+  for (size_t i = 0; i < plonk.h_len; i++) {
+    plonk.k2_h[i] = hf_mul(plonk.h[i], K2);
+  }
+
+  // build h_pows matrix (vandermonde matrix) and compute its inverse
+  MATRIX h_pows = matrix_zero(plonk.h_len, plonk.h_len);
+  for (size_t c = 0; c < h_pows.n; c++) {
+    for (size_t r = 0; r < h_pows.m; r++) {
+      matrix_set(&h_pows, r, c, hf_pow(plonk.h[r], c));
+    }
+  }
+  plonk.h_pows_inv = matrix_inv(&h_pows);
+  matrix_free(&h_pows);
+
+  // compute z_h_x = Poly::z(h)
+  plonk.z_h_x = poly_z(plonk.h, plonk.h_len);
 
   return plonk;
 }
